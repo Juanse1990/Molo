05c7d9d241e7770999eefcddc8803b29b9c44972
Juan Sebastian Morel Ruiz Diaz <juanse1990@live.com.ar>
2023-07-21 17:22:16 -0300

    fix: color hover navbar

--- src/components/NavBar/Menu.jsx
+++ src/components/NavBar/Menu.jsx
@@ -5,19 +5,19 @@ const Menu = () => {
 		<>
 			<nav className=' hidden w-[500px] lg:block'>
 				<ul className='flex justify-between text-[1.2em] font-bold'>
-					<li className='h-[112px] pt-[42px] hover:border-b-4 hover:border-amarillo'>
+					<li className='h-[112px] pt-[42px] hover:border-b-4 hover:border-azul'>
 						<Link>Hombre</Link>
 					</li>
-					<li className='h-[112px] pt-[42px] hover:border-b-4 hover:border-amarillo'>
+					<li className='h-[112px] pt-[42px] hover:border-b-4 hover:border-azul'>
 						<Link>Mujer</Link>
 					</li>
-					<li className='h-[112px] pt-[42px] hover:border-b-4 hover:border-amarillo'>
+					<li className='h-[112px] pt-[42px] hover:border-b-4 hover:border-azul'>
 						<Link>Unisex</Link>
 					</li>
-					<li className='h-[112px] pt-[42px] hover:border-b-4 hover:border-amarillo'>
+					<li className='h-[112px] pt-[42px] hover:border-b-4 hover:border-azul'>
 						<Link>Nosotros</Link>
 					</li>
-					<li className='h-[112px] pt-[42px] hover:border-b-4 hover:border-amarillo'>
+					<li className='h-[112px] pt-[42px] hover:border-b-4 hover:border-azul'>
 						<Link>Contacto</Link>
 					</li>
 				</ul>
